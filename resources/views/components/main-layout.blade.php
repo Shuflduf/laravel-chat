82865ed199c6fff017b329a432ad672fbd17a948
Shuflduf <shuflduf@shuflduf.xyz>
2025-04-25 19:49:17 -0600

    vercel test

--- resources/views/components/main-layout.blade.php
+++ resources/views/components/main-layout.blade.php
@@ -1,7 +1,7 @@
 <div class="flex min-h-screen w-full bg-slate-300">
     <div class="flex flex-col gap-4 w-30 m-4 mr-0 sticky h-[calc(100vh-2rem)]">
         <div class="bg-slate-500 rounded-lg">
-            <p class="text-xl font-bold text-white m-2">@Shuflduf</p>
+            <p class="text-xl font-bold text-white m-2">@shuflduf</p>
         </div>
         <div class="flex flex-col bg-slate-500 h-full rounded-lg">
             @foreach ($conversations as $convo)
