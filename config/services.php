4627caea8888552f5a267ddf9f9ce92a3f58b81f
Shuflduf <shuflduf@shuflduf.xyz>
2025-04-28 13:08:44 -0600

    asdahsdasdas

--- config/services.php
+++ config/services.php
@@ -16,7 +16,7 @@ return [
     'github' => [
         'client_id' => env('GITHUB_CLIENT_ID'),
         'client_secret' => env('GITHUB_CLIENT_SECRET'),
-        'redirect' => env('APP_URL').'/auth/callback',
+        'redirect' => env('GITHUB_REDIRECT_URI', env('APP_URL').'/auth/callback'),
     ],
 
     'postmark' => [
